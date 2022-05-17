2013c2c1e23c38e00d5cb8fee6eecc2d5f93e4d0
hnyophai <102907527+hnyophai@users.noreply.github.com>
2022-05-17 15:43:47 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,22 +10,21 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/4d412495ab546f9062898.jpg",
-        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝗖𝗮𝗻𝗱𝘆𝘅𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [𝗖𝗔𝗡𝗗𝗬](https://t.me/C_A_N_D_Y_O_P)
+        photo=f"https://telegra.ph/file/a15c311b44cc0e696f20b.jpg",
+        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝗖𝗮𝗻𝗱𝘆𝘅𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [HNY](https://t.me/HNYOP)
 
-𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [ 𝗖𝗔𝗡𝗗𝗬](https://t.me/C_A_N_D_Y_O_P)
-𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [𝗚𝗥𝗢𝗨𝗣](https://t.me/DOSTO_KI_ZOPDI)
-𝗢𝗪𝗡𝗘𝗥 :- [𝗖𝗔𝗡𝗗𝗬 ](https://t.me/C_A_N_D_Y_O_P)
-𝐒𝐨𝐮𝐫𝐜𝐞  :- [✨  𝗖𝗹𝗶𝗰𝗸 ☑️ 𝗥𝗲𝗽𝗼 🌍](https://t.me/DOSTO_KI_ZOPDI)
-𝗞𝗜𝗧𝗞𝗔𝗧 :- [𝗕𝗔𝗕𝗬](https://t.me/K_I_T_K_A_T)
-𝗛𝗡𝗬 :- [𝗝𝗔𝗔𝗡](https://t.me/HNYOP)
+𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [HNY](https://t.me/HNYOP)
+𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [𝗚𝗥𝗢𝗨𝗣](https://t.me/MUSIC_WORLDS_OP)
+𝗢𝗪𝗡𝗘𝗥 :- [BROKEN](https://t.me/S12K_GAMER_YT_OP)
+FIGHTING:- [GROUP](https://t.me/Dangerousfighters)
+UPDATES :- [CHANNEL](https://t.me/HNYROBO)
 
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝗖𝗔𝗡𝗗𝗬](https://t.me/C_A_N_D_Y_O_P)**""",
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [BROKEN](https://t.me/S12K_GAMER_YT_OP)**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/DOSTO_KI_ZOPDI")
+                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/MUSIC_WORLDS_OP")
                 ]
                 
            ]
@@ -36,13 +35,13 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/4d412495ab546f9062898.jpg",
+        photo=f"https://te.legra.ph/file/a15c311b44cc0e696f20b.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 Cʟɪᴄᴋ Mᴇ Tᴏ Gᴇᴛ Rᴇᴘᴏ 💞", url=f"https://t.me/DOSTO_KI_ZOPDI")
+                        "💥 Cʟɪᴄᴋ Mᴇ Tᴏ Gᴇᴛ Rᴇᴘᴏ 💞", url=f"https://t.me/MUSIC_WORLDS_OP")
                 ]
             ]
         ),
