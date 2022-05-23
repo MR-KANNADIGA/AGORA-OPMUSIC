6749d7fdf9bc5b2f7e0da2e379de057f1f783262
KARUNADA KING <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-05-23 10:06:44 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply("𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 🎧 𝐅𝐫𝐨𝐦 Toxic ❤️ 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
+    m = message.reply("𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 🎧 𝐅𝐫𝐨𝐦 𝐁𝐢𝐠 𝐁𝐫𝐨𝐭𝐡𝐞𝐫 𝐀𝐠𝐨𝐫𝐚 ❤️ 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ✨ 𝐒𝐨𝐧𝐠 🎸 𝐅𝐫𝐨𝐦 Toxic🥀 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
+    m.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ✨ 𝐒𝐨𝐧𝐠 🎸 𝐅𝐫𝐨𝐦 𝐁𝐢𝐠 𝐁𝐫𝐨𝐭𝐡𝐞𝐫 𝐀𝐠𝐨𝐫𝐚🥀 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ [Toxic](@wtf_toxicop) ❤️**"
+        rep = "**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ [𝐁𝐢𝐠 𝐁𝐫𝐨𝐭𝐡𝐞𝐫](https://t.me/mr_agora) ❤️**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("**𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌ 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀 [Toxic](https://t.me/wtf_toxicop) ❤️**")
+        m.edit("**𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌ 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀 [𝐁𝐢𝐠 𝐁𝐫𝐨𝐭𝐡𝐞𝐫](https://t.me/mr_agora) ❤️**")
         print(e)
 
     try:
