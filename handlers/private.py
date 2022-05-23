0954a1e18fd3782454113caf96c6572107c5bc2c
KARUNADA KING <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-05-23 10:26:20 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -23,7 +23,7 @@ async def start_(client: Client, message: Message):
                 [
                     InlineKeyboardButton(
                         "𝗕𝗥𝗢𝗧𝗛𝗘𝗥𝗦 𝗖𝗟𝗨𝗕", url=f"https://t.me/BROTHERS_TERRITORY")
-                ]
+                ]h
                 
            ]
         ),
@@ -40,7 +40,12 @@ async def help(client: Client, message: Message):
                 [
                     InlineKeyboardButton(
                         "𝙍𝙚𝙥𝙤", url=f"https://t.me/agoraempire")
-                ]
-            ]
+                  ]
+                
+           ]
         ),
     )
+                
+o
+ 
+  
